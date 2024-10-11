778788aef392b2159e5691edd1e3822e8a92035c
MD Tariqul Islam <ittarek551@gmail.com>
2024-10-11 18:34:45 +0600

    add responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,26 @@ import video from "../src/assets/video2.mp4"
 function App() {
 
 //  position: absolute;
-
+// flex flex-col  mx-auto w-full lg:w-[450px]  lg:mt-6  h-[100vh] text-white px-4 pb-4  bg-black   overflow-auto touch-auto scroll-smooth
   return (
-    <main className="flex flex-col justify-evenly items-center  h-screen mx-auto  ">
+    <div
+      className="flex flex-col justify-between items-center mx-auto w-full lg:w-[450px]   h-[100vh] overflow-auto absolute bottom-0 left-0 right-0 
+     "
+    >
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="background-video h-full w-full top-0 left-0 absolute object-cover -z-10"
+        className="background-video w-full lg:w-[450px]  h-[100vh]  absolute mx-auto  bottom-0  object-cover -z-10 "
       >
         <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
+    
       </video>
       <div className="overlay"></div>
 
       {/* Top Logo */}
-      <div className=" mx-auto text-center">
+      <div className=" mx-auto text-center mt-16 z-10">
         <img src={topLogo} className="" alt="logo" />
         <p className="text-sm uppercase  text-black text-semibold">
           Lorem Ipsum
@@ -40,26 +43,31 @@ function App() {
       </div>
 
       {/* Bottom Content */}
-      <div className="text-center flex flex-col justify-between items-center ">
-        <div className="flex items-center flex-col justify-center text-[#fff] mb-8">
+      <div className="text-center flex flex-col  items-center mb-11">
+        {/*  */}
+        <div className="flex items-center flex-col justify-center text-[#fff] mb-9">
           <img src={girlImg} className="mb-2" alt="girl" />
 
           <h3 className="">
             {" "}
             <p className="text-sm">lorem by</p>
-            <p className='font-bold'>Lorem & Ipsum lorem</p>
+            <p className="font-bold">Lorem & Ipsum lorem</p>
           </h3>
         </div>
-        <div className="flex items-center justify-between  bg-[#4C5555] text-[#FFFFFF] rounded-full border border-[#3a3a3a] p-4 w-[200px] my-2  font-serif btn_div">
-          <button> + Lorem</button>
-          <button> + ipsum</button>
+        {/*  */}
+        <div className="  font-serif btn_div ">
+          <div className=" bg-[#4C5555] text-[#FFFFFF] rounded-full border border-[#3a3a3a] p-4 w-[200px] ">
+            <button> + Lorem</button>
+            <button> + ipsum</button>
+          </div>
+          <p className="text-sm text-[#ffff] my-1">27 + lorem </p>
         </div>{" "}
-        <p className='text-sm text-[#fff] my-1'>27 + lorem</p>
-        <div className=' absolute bottom-0 '>
+        {/* line */}
+        <div className="absolute bottom-0  ">
           <img src={line} alt="line" />
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 
